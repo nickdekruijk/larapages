2a6340767b154bf96600ca2b74d2c3a1bbf857aa
Nick de Kruijk <nick@MacBook-Pro-2.local>
2016-04-21 16:30:36 +0200

    Fix menu with start + end

--- src/LaraPagesController.php
+++ src/LaraPagesController.php
@@ -24,7 +24,7 @@ class LaraPagesController extends Controller
         $i=0;
         foreach(config('larapages.models') as $model=>$title) {
             $i++;
-            $this->nav.='<li class="'.($i==1?'start':'').($i==count(config('larapages.models'))?'end':'').($model==$this->modelId?' active':'').'"><a href="/'.config('larapages.adminpath').'/model/'.$model.'">'.$title.'</li>';
+            $this->nav.='<li class="'.($i==1?'start':'').($i==count(config('larapages.models'))?' end':'').($model==$this->modelId?' active':'').'"><a href="/'.config('larapages.adminpath').'/model/'.$model.'">'.$title.'</li>';
         }
         if (config('larapages.media'))
            $this->nav.='<li class="start end logout'.($this->modelId=='media'?' active':'').'"><a href="/'.config('larapages.adminpath').'/media/">'.config('larapages.media.nicename', 'Media').'</a></li>';
