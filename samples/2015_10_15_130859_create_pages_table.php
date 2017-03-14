a897caab5ef21dd9d70fe3de0599c85e56fee85b
Nick de Kruijk <git@nickdekruijk.nl>
2017-03-14 14:57:19 +0100

    PSR-2 Formatting on the sample files

--- samples/2015_10_15_130859_create_pages_table.php
+++ samples/2015_10_15_130859_create_pages_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePagesTable extends Migration
 {
@@ -15,26 +15,26 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->increments('id');
-            
+
             $table->integer('parent')->nullable()->unsigned();
             $table->boolean('active')->default(1);
             $table->boolean('hidden')->default(0);
             $table->boolean('home')->default(0);
-            $table->string('title',100);
-            $table->string('view',100)->nullable();
+            $table->string('title', 100);
+            $table->string('view', 100)->nullable();
             $table->string('head')->nullable();
-            $table->string('html_title',65)->nullable();
-            $table->string('slug',100)->nullable();
+            $table->string('html_title', 65)->nullable();
+            $table->string('slug', 100)->nullable();
             $table->text('description')->nullable();
             $table->date('date')->nullable();
             $table->longText('pictures')->nullable();
             $table->string('background')->nullable();
             $table->longText('body')->nullable();
             $table->integer('sort')->default(0)->unsigned();
-            
+
             $table->softDeletes();
-			$table->timestamps();
-            
+            $table->timestamps();
+
             $table->index(['active', 'parent', 'sort']);
             $table->foreign('parent')->references('id')->on('pages');
         });
--- samples/Page.php
+++ samples/Page.php
@@ -9,43 +9,43 @@ use Request;
 class Page extends Model
 {
     use SoftDeletes;
-    
+
     # Define what the PagesAdminController is allowed to to with the model and how
-    public $pagesAdmin=[
-        'nicename'=>'Website pages',                # Title to show in header
-        'index'=>'title,id',                        # Columns to show in listview
-        'active'=>'active',                         # Boolean type column that determines if page is active or not
-        'sortable'=>'sort',                         # Data can be sorted by dragging, store values in 'sort' column
-        'orderBy'=>'sort',                          # Data is ordered by this column at for descending order you can use something like 'date DESC'
-        'treeview'=>'parent',                       # Items can be shown in a treeview, 'parent' column determines parent/child relation
-        'expanded'=>3,                              # When treeview is shown auto expand up to 3 levels
-        'validate'=>[                               # Laravel validation rules
-            'title'=>'required',
-            'date'=>'date|nullable',
+    public $pagesAdmin = [
+        'nicename' => 'Website pages', # Title to show in header
+        'index' => 'title,id', # Columns to show in listview
+        'active' => 'active', # Boolean type column that determines if page is active or not
+        'sortable' => 'sort', # Data can be sorted by dragging, store values in 'sort' column
+        'orderBy' => 'sort', # Data is ordered by this column at for descending order you can use something like 'date DESC'
+        'treeview' => 'parent', # Items can be shown in a treeview, 'parent' column determines parent/child relation
+        'expanded' => 3, # When treeview is shown auto expand up to 3 levels
+        'validate' => [# Laravel validation rules
+        'title' => 'required',
+            'date' => 'date|nullable',
         ],
-        'accessors'=>false,                         # Disable accessors when editing model. Use this when accessors modify empty columns for example and you want to leave them blank when editing
-        'type'=>[                                   # Column types, this determines the model editing view input types. If ommitted default text input is used
-            'active'=>'boolean',
-            'hidden'=>'boolean',
-            'home'=>'boolean',
-            'title'=>'100',
-            'view'=>'100',
-            'slug'=>'100',
-            'html_title'=>'64',
-            'description'=>'text',
-            'date'=>'date',
-            'pictures'=>'media,10',
-            'background'=>'media',
-            'body'=>'longtext',
+        'accessors' => false, # Disable accessors when editing model. Use this when accessors modify empty columns for example and you want to leave them blank when editing
+        'type' => [# Column types, this determines the model editing view input types. If ommitted default text input is used
+        'active' => 'boolean',
+            'hidden' => 'boolean',
+            'home' => 'boolean',
+            'title' => '100',
+            'view' => '100',
+            'slug' => '100',
+            'html_title' => '64',
+            'description' => 'text',
+            'date' => 'date',
+            'pictures' => 'media,10',
+            'background' => 'media',
+            'body' => 'longtext',
         ],
-        'rename'=>[                                 # Rename columns
-            'pictures'=>'Picture',
+        'rename' => [# Rename columns
+        'pictures' => 'Picture',
         ],
-        'tinymce'=>[                                # List of columns that can contain html and should be edited with TinyMCE
-            'body'=>'tinymce options',
+        'tinymce' => [# List of columns that can contain html and should be edited with TinyMCE
+        'body' => 'tinymce options',
         ],
     ];
-    
+
     # Fillable columns, also used by PagesAdminController to build the form so the order matters
     protected $fillable = [
         'active',
@@ -62,48 +62,56 @@ class Page extends Model
         'background',
         'body',
     ];
-    
+
     /**
      * The attributes that should be mutated to dates.
      *
      * @var array
      */
     protected $dates = ['date', 'deleted_at', 'published_at'];
-    
+
     # This scope returns only the active pages and in the right order
     public function scopeActiveSorted($query)
     {
         $query->where('active', 1)->orderBy('sort');
     }
-    
+
     # This scope return only the active pages that belong to a certain parent and in the right order
-    public function scopeParent($query,$parent)
+    public function scopeParent($query, $parent)
     {
         $query->where('parent', $parent)->activeSorted();
     }
-    
+
     # If head is empty use the title
     public function getHeadAttribute($value)
     {
-        if (!$value) $value=$this->title;
+        if (!$value) {
+            $value = $this->title;
+        }
         return $value;
     }
-    
+
     # If html_title is empty use the title
     public function getHtmlTitleAttribute($value)
     {
-        if (!$value) $value=$this->title;
+        if (!$value) {
+            $value = $this->title;
+        }
         return $value;
     }
-    
+
     # If slug is empty create slug based on title
     public function getSlugAttribute($value)
     {
         # If slug = / then it's actually an empty route
-        if ($value=='/') return '';
-        
+        if ($value == '/') {
+            return '';
+        }
+
         # No value so create nicely formatted slug from title
-        if (!$value) $value=str_slug($this->title);
+        if (!$value) {
+            $value = str_slug($this->title);
+        }
 
         return $value;
     }
@@ -111,18 +119,20 @@ class Page extends Model
     # Determine fullUrl by include the parent slug(s)
     public function getFullUrlAttribute()
     {
-        if ($this->parent>0) {
-            $parent=Page::findOrFail($this->parent);
-            return $parent->fullUrl.'/'.$this->slug;
-        } else
+        if ($this->parent > 0) {
+            $parent = Page::findOrFail($this->parent);
+            return $parent->fullUrl . '/' . $this->slug;
+        } else {
             return $this->slug;
+        }
     }
 
     # If picture is empty return first picture from pictures column
     public function getPictureAttribute($value)
     {
-        if (!$value)
-            $value=trim(explode(chr(10), trim($this->pictures))[0]);
+        if (!$value) {
+            $value = trim(explode(chr(10), trim($this->pictures))[0]);
+        }
         return $value;
     }
 
@@ -135,52 +145,61 @@ class Page extends Model
     # Fetch the navigation tree, usefull for pages not using the @route menthod
     public static function navigation()
     {
-        $page=new Page;
+        $page = new Page;
         return $page->walk();
     }
 
     # Placeholder for currentPage set bij walk() method
-    public $currentPage=false;
+    public $currentPage = false;
 
     /**
      * Walk thru the pages tree and return the navigation html and set currentPage is found
      *
      * @return (string)$nav
      */
-    public function walk($parent=null, $depth=0, $ids=false, $url='/', $hidden=false, $unhide=false) {
+    public function walk($parent = null, $depth = 0, $ids = false, $url = '/', $hidden = false, $unhide = false)
+    {
         # The id might not exist if it's the domain root for example
-        if (!isset($ids[$depth])) $ids[$depth]='';
-        
+        if (!isset($ids[$depth])) {
+            $ids[$depth] = '';
+        }
+
         # Fetch all pages
         $pages = \App\Page::parent($parent)->get();
         # Return if no pages found to prevent empty navigation <ul></ul>
-        if (!count($pages)) return;
+        if (!count($pages)) {
+            return;
+        }
 
         # Create the navigation html
-        $nav = '<ul class="nav'.$depth.'">';
-        
-        foreach($pages as $page) {
+        $nav = '<ul class="nav' . $depth . '">';
+
+        foreach ($pages as $page) {
             # Set currentPage if it's the one but only if $depth equals the actual amount of ids
-            if ($ids[$depth]==$page->slug && $depth==count($ids)-1) $this->currentPage=$page;
-            
+            if ($ids[$depth] == $page->slug && $depth == count($ids) - 1) {
+                $this->currentPage = $page;
+            }
+
             # Add page to navigation html and add active class when needed
-             if (!$page->toArray()['hidden'] || $unhide) {
-                $nav.=' <li class="'.($ids[$depth]==$page->slug?'active':'').($page->toArray()['hidden']?' hidden':'').'">';
-                $nav.='<a href="'.($url.$page->slug).'">'.$page->title.'</a>';
-             }
+            if (!$page->toArray()['hidden'] || $unhide) {
+                $nav .= ' <li class="' . ($ids[$depth] == $page->slug ? 'active' : '') . ($page->toArray()['hidden'] ? ' hidden' : '') . '">';
+                $nav .= '<a href="' . ($url . $page->slug) . '">' . $page->title . '</a>';
+            }
             # Check if the page has subpages and add them
-            $nav.=Page::walk($page->id, $depth+1, $ids, $url.$page->slug.'/', $page->toArray()['hidden'] || $hidden);
+            $nav .= Page::walk($page->id, $depth + 1, $ids, $url . $page->slug . '/', $page->toArray()['hidden'] || $hidden);
 //            if (isset($walk[1]) && $walk[1]) $currentPage=$walk[1];
-            
+
             # Finalize navigation html
-            if (!$page->toArray()['hidden'])
-                $nav.='</li>';
+            if (!$page->toArray()['hidden']) {
+                $nav .= '</li>';
+            }
         }
-        
+
         # Finalize navigation html and return it
-        $nav.='</ul>';
-        if (!$hidden)
+        $nav .= '</ul>';
+        if (!$hidden) {
             return $nav;
+        }
     }
 
     /**
@@ -194,11 +213,14 @@ class Page extends Model
         $navigationHtml = $this->walk(null, 0, Request::segments());
 
         # If currentPage isn't set raise a custom 404
-        if (!$this->currentPage) abort(404);
-        
-        # Return the page view
-        if ($this->currentPage['parent']>0) $this->currentPage['view']='detail';
-		return view($this->currentPage['view']?$this->currentPage['view']:'page', ['page' => $this->currentPage, 'navigationHtml' => $navigationHtml]);
-    }
+        if (!$this->currentPage) {
+            abort(404);
+        }
 
+        # Return the page view
+        if ($this->currentPage['parent'] > 0) {
+            $this->currentPage['view'] = 'detail';
+        }
+        return view($this->currentPage['view'] ? $this->currentPage['view'] : 'page', ['page' => $this->currentPage, 'navigationHtml' => $navigationHtml]);
+    }
 }
