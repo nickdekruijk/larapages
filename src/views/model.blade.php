03f8c6a2fe6cf4fa2b21a228fa1c3aa3a0e20b26
Nick de Kruijk <git@nickdekruijk.nl>
2017-03-21 19:14:27 +0100

    content_css / tinymce_css also with asset()

--- src/views/model.blade.php
+++ src/views/model.blade.php
@@ -119,7 +119,7 @@
     	    theme: 'modern',
     	    menubar: false,
     	    paste_as_text: true,
-    	    content_css: "{{ isset($model->pagesAdmin['tinymce_css'])?$model->pagesAdmin['tinymce_css']:'/vendor/larapages/css/tinymce.css' }}",
+    	    content_css: "{{ asset(isset($model->pagesAdmin['tinymce_css'])?$model->pagesAdmin['tinymce_css']:'/vendor/larapages/css/tinymce.css') }}",
             browser_spellcheck: true,
             convert_urls : false,
 			file_browser_callback: function(input_id, input_value, type, win){
