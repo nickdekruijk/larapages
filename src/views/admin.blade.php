1584496ef79135de6130943e88f4cbed671c1cbd
Nick de Kruijk <git@nickdekruijk.nl>
2018-01-11 14:19:05 +0100

    Revert changing <title>

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -6,9 +6,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="robots" content="NOINDEX, NOFOLLOW">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable = no">
-	<title>
-		{{ str_replace('www.','',$_SERVER['HTTP_HOST']) }} - Admin @yield('title')
-	</title>
+	<title>{{ str_replace('www.','',$_SERVER['HTTP_HOST']) }} - Admin @yield('title')</title>
 	<link href="{{asset('/vendor/larapages/css/admin.css')}}" rel="stylesheet">
 	@if (config('larapages.css'))
 	@foreach (config('larapages.css') as $css)
